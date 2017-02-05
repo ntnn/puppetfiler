8d1f54995325dac69d7afa7f632d21a1955bc9fc
Nelo Wallus <nelo@wallus.de>
2017-02-05 17:35:11 +0100

    Move mock of PuppetForge::Module to spec_helper

--- spec/puppetfiler/mod_spec.rb
+++ spec/puppetfiler/mod_spec.rb
@@ -6,18 +6,6 @@ require 'puppetfiler/mod'
 describe Puppetfiler::Mod do
     describe '::newest' do
         it 'returns the newest version' do
-            mod = double(
-                'PuppetForge::Module',
-                :current_release => double(
-                    'PuppetForge::Release',
-                    :version => '4.15.0'
-                )
-            )
-
-            expect(PuppetForge::Module)
-                .to receive(:find)
-                .and_return(mod)
-
             expect(Puppetfiler::Mod.newest('puppetlabs/stdlib'))
                 .to eq(SemanticPuppet::Version.parse('4.15.0'))
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,19 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "puppetfiler"
+require 'puppetfiler'
+require 'rspec/expectations'
+
+RSpec.configure do |config|
+    config.before(:each) do
+        @mod = double(
+            'PuppetForge::Module',
+            :current_release => double(
+                'PuppetForge::Release',
+                :version => '4.15.0'
+            )
+        )
+
+        allow(PuppetForge::Module)
+            .to receive(:find)
+            .and_return(@mod)
+    end
+end
