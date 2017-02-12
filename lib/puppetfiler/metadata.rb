28dff809c280ed0752fe1e8f82d31a30d0bc0f6c
Nelo Wallus <nelo@wallus.de>
2017-02-12 23:51:23 +0100

    Remove TODO, missed before

--- lib/puppetfiler/metadata.rb
+++ lib/puppetfiler/metadata.rb
@@ -14,23 +14,6 @@ module Puppetfiler
 
         def fixture(modifiers = {})
             Puppetfiler.fixture(@dependencies, {}, modifiers)
-            # TODO: Move fixture methdod out of puppetfile class to
-            # parent class
-            #
-            # modules/repos should be pass-able values
-            #
-            # An invocation from the puppetfile class would look like
-            #
-            #   Puppetfiler.fixture(@modules, @repos, modifier)
-            #
-            # and from this class
-            #
-            #   Puppetfiler.fixture(@dependencies, {})
-            #
-            # However this requires that the way forge modules are
-            # handles is changed a bit.
-            # I think the best way would be to modify Puppetfiler::Mod
-            # to fit the needs,
         end
 
         def eql?(other)
