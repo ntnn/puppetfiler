efa9692b56078292463ab2f27faa9d27242e62bc
Nelo Wallus <nelo@wallus.de>
2017-02-13 01:22:15 +0100

    Rewrite cli to allow metadata and puppetfile actions through the same interface

--- lib/puppetfiler/cli.rb
+++ lib/puppetfiler/cli.rb
@@ -5,22 +5,56 @@ require 'puppetfiler/version'
 
 module Puppetfiler
     class CLI < Thor
-        desc 'check [puppetfile]', 'Check forge for newer versions of used forge modules'
-        def check(puppetfile = 'Puppetfile')
-            pf = Puppetfiler::Puppetfile.new(puppetfile)
-            format = "% -#{pf.maxlen_name}s  % -#{pf.maxlen_ver}s  %s"
+        class_option :puppetfile, {
+            :default => nil,
+            :desc    => 'Puppetfile to act on',
+            :aliases => ['p'],
+        }
+
+        class_option :metadata, {
+            :default => nil,
+            :desc    => 'metadata.json to act on',
+            :aliases => ['m'],
+        }
+
+        desc 'check', 'Check forge for newer versions of used forge modules'
+        def check()
+            target = target(options)
+
+            case target[:type]
+            when :puppetfile
+                t = Puppetfiler::Puppetfile.new(target[:result])
+            when :metadata
+                # TODO see below
+                fail 'Checking metadata.json for version range updates is not implemented yet'
+            else fail "Unkown type: #{target[:type]}"
+            end
+
+            format = "% -#{t.maxlen_name}s  % -#{t.maxlen_ver}s  %s"
 
             puts sprintf(format, 'module', 'current', 'newest')
 
-            pf.updates.each do |name, hash|
+            # TODO the updates should be collected asynchronously to
+            # speed up the process
+            t.updates.each do |name, hash|
                 puts sprintf(format, name, hash[:current], hash[:newest])
             end
         end
 
-        desc 'fixture [puppetfile]', 'Create puppetlabs_spec_helper compatible .fixtures.yml from puppetfile'
+        desc 'fixture', 'Create puppetlabs_spec_helper compatible .fixtures.yml from puppetfile or metadata.json'
         method_option :stdout, :aliases => '-o'
-        def fixture(puppetfile = 'Puppetfile')
-            f = Puppetfiler::Puppetfile.new(puppetfile).fixture.to_yaml
+        def fixture()
+            target = target(options)
+
+            case target[:type]
+            when :puppetfile
+                f = Puppetfiler::Puppetfile.new(target[:result])
+            when :metadata
+                f = Puppetfiler::Metadata.new(File.new(target[:result]))
+            else fail "Unkown type: #{target[:type]}"
+            end
+
+            f = f.fixture.to_yaml
 
             if options[:stdout]
                 puts f
@@ -32,6 +66,22 @@ module Puppetfiler
         desc 'version', 'Output version'
         def version
             puts "puppetfiler v#{Puppetfiler::VERSION}"
+        end
+
+        private
+        def target(opts)
+            {
+                :puppetfile => 'Puppetfile',
+                :metadata   => 'metadata.json',
+            }.each do |sym, str|
+                if opts[sym]
+                    return { :result => opts[sym], :type => sym }
+                elsif File.exists?(str)
+                    return { :result => str, :type => sym }
+                end
+            end
+
+            fail 'No Puppetfile or metadata.json found, aborting'
         end
     end
 end
